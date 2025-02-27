1390c42ad08bac936c54529776a407122a8d607a
my name <banbunrong123@gmail.com>
2025-02-27 16:03:00 +0100

    Update Ford Fulkerson Algorthms.

--- code/main.py
+++ code/main.py
@@ -10,7 +10,7 @@ from Graph import build_flow_network
 # data_path = "D:/Document_Ecole/Y1_ENSAE/Semestre_2/Programmation/ensae-prog25/input/"
 
 # file_name = data_path + "grid01.in"
-file_name = "D:/Document_Ecole/Y1_ENSAE/Semestre_2/Programmation/ensae-prog25/input/" + "grid02.in"
+file_name = "D:/Document_Ecole/Y1_ENSAE/Semestre_2/Programmation/ensae-prog25/input/" + "grid01.in"
 grid = Grid.grid_from_file(file_name)
 print(grid)
 
@@ -29,7 +29,7 @@ print(f"The final score of SolverGreedy is: {solver.score()}")
 
 graph, source, sink = build_flow_network(grid)
 max_matching = graph.ford_fulkerson(source, sink)
-
+# print(build_flow_network(grid))
 print(f" Maximum number of valid pairs: {max_matching}")
 
-grid.plot()
+# grid.plot()
